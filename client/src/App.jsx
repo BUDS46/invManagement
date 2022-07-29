f433dddbcded1bb4f67cf3467ead0a972a747cc1
davidgetzen <98428144+davidgetzen@users.noreply.github.com>
2022-07-28 17:52:20 -0700

    more changes to front end but still has rendering errors

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,67 +1,72 @@
-import React, { Fragment } from 'react';
-import {
-  HashRouter as Router, Route, Link, Switch, UseHistory,
-} from 'react-router-dom';
+/* eslint-disable */
+import React, { Fragment, Component } from 'react';
+import { BrowserRouter as Router, Route, Link, Switch, UseHistory } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
 
-export default function App() {
-  return (
-    <Router>
-      <main>
-        <nav>
-          <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/about">About</Link></li>
-            <li><Link to="/device">Device</Link></li>
-          </ul>
-        </nav>
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <main>
+          <Nav>
+            <h1>Welcome to the website.</h1>
+            <ul>
+              <li><Link to={"/"}>Home</Link></li>
+              <li><Link to={"/about"}>About</Link></li>
+              <li><Link to={"/contact"}>Contact</Link></li>
+            </ul>
+          </Nav>
 
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/about" component={About} />
-          <Route path="/device" component={Device} />
-          <Route render={() => <h1>404: page not found</h1>} />
-        </Switch>
-      </main>
-    </Router>
-  );
+          <Switch>
+            <Route path="/" exact element={Home} />
+            <Route path="/about" element={About} />
+            <Route path="/contact" element={Contact} />
+            <Route render={() => <h1>404: page not found</h1>} />
+          </Switch>
+        </main>
+      </Router>
+    );
+  }
 }
-
-// Home page
-function Home() {
-  return (
-    <>
-      <h1>Home</h1>
-      <Faketext />
-    </>
-  );
-}
-// About page
-function About() {
-  const history = UseHistory();
-  return (
-    <>
-      <h1>About</h1>
-      <button onClick={() => history.push('/')}> Go to home page </button>
-      <Faketext />
-    </>
-  );
-}
-// Page for data input - use of "device"/database storage
-function Device() {
-  const history = UseHistory();
-  return (
-    <>
-      <h1>Device</h1>
-      <button onClick={() => history.push('/')}> Go to home page </button>
-      <Faketext />
-    </>
-  );
-}
-// Placeholder text constant
-function Faketext() {
-  return (
-    <p>
-      Placeholder text until additional development.
-    </p>
-  );
-}
+export default App;
+// // Home page
+// function Home() {
+//   return (
+//     <>
+//       <h1>Home</h1>
+//       <Faketext />
+//     </>
+//   );
+// }
+// // About page
+// function About() {
+//   const history = UseHistory();
+//   return (
+//     <>
+//       <h1>About</h1>
+//       <button type="button" onClick={() => history.push('/')}> Go to home page </button>
+//       <Faketext />
+//     </>
+//   );
+// }
+// // Page for data input - use of "device"/database storage
+// function Device() {
+//   const history = UseHistory();
+//   return (
+//     <>
+//       <h1>Device</h1>
+//       <button type="button" onClick={() => history.push('/')}> Go to home page </button>
+//       <Faketext />
+//     </>
+//   );
+// }
+// // Placeholder text constant
+// function Faketext() {
+//   return (
+//     <p>
+//       Placeholder text until additional development.
+//     </p>
+//   );
+// }
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,14 +1,15 @@
+/* eslint-disable */
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(
   <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
+
+    <App />
+
   </React.StrictMode>,
 );
